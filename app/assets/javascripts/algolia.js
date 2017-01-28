0cb6127d1d9035d7cb4ad8f063073d52b499e00a
rhamill1 <rhamill11@gmail.com>
2017-01-28 15:57:46 -0800

    mvp for autocomplete

--- app/assets/javascripts/algolia.js
+++ app/assets/javascripts/algolia.js
@@ -5,26 +5,20 @@ $(document).ready(function() {
   var client = algoliasearch('', '');
   var index = client.initIndex('Tweet');
 
+  autocomplete('#aa-search-input',
+    { hint: false }, {
+      source: autocomplete.sources.hits(index, {hitsPerPage: 5}),
+      displayKey: 'tweet',
+      templates: {
 
-  $('#singlebutton').on('click', function(e) {
-    e.preventDefault();
-    $('#tweets').empty();
+        suggestion: function(suggestion) {
 
-    var searchString = $('#textinput').serialize();
+          var tweetText = suggestion.text
 
-    index.search(searchString.substring(10), { hitsPerPage: 11, page: 0 })
-      .then(function searchDone(content) {
-        var hitsArray = content.hits;
-
-        for (var tweetIndex = hitsArray.length - 1; tweetIndex > 0; tweetIndex--) {
-          var tweetText = hitsArray[tweetIndex].text
-          // console.log(tweetText);
           if (tweetText.indexOf('https://t.co/') >= 0 ) {
-            // console.log('this has an image');
-            // console.log(tweetText.indexOf('https://t.co/'));
+
             var tweetTextIndexImage = tweetText.indexOf('https://t.co/');
             var imageHtml = tweetText.slice(tweetTextIndexImage, -1);
-            console.log(imageHtml);
             var tweetHtml =
               '  <div class="tweet">' +
               '  <a href='+ imageHtml + '"/>' + imageHtml + '</a>' +
@@ -32,27 +26,17 @@ $(document).ready(function() {
               '  </div>'
 
           } else {
-            // console.log('NO image');
             var tweetHtml =
               '  <div class="tweet">' +
               '    <h5>' + tweetText + '</h5>' +
               '  </div>'
           };
 
-
-
-
-          // console.log(hitsArray[tweetIndex].text);
-          $('#tweets').append(tweetHtml);
+          $('#tweets').prepend(tweetHtml);
         }
 
-        // $('#tweets').css( 'background-color', 'red' );
-
-      })
-      .catch(function searchFailure(err) {
-        console.error(err);
-      });
-
-  });
+      }
+    }
+  );
 
 });
