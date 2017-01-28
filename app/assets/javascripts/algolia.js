6f7db51b931b7e4253bce927db5444e45a1f8899
rhamill1 <rhamill11@gmail.com>
2017-01-28 13:13:39 -0800

    adds basic styling

--- app/assets/javascripts/algolia.js
+++ app/assets/javascripts/algolia.js
@@ -11,15 +11,41 @@ $(document).ready(function() {
 
     var searchString = $('#textinput').serialize();
 
-    index.search(searchString.substring(10), { hitsPerPage: 10, page: 0 })
+    index.search(searchString.substring(10), { hitsPerPage: 100, page: 0 })
       .then(function searchDone(content) {
         var hitsArray = content.hits;
 
         for (var tweetIndex = hitsArray.length - 1; tweetIndex > 0; tweetIndex--) {
-          $('#tweets').append(hitsArray[tweetIndex].text);
+          var tweetText = hitsArray[tweetIndex].text
+          // console.log(tweetText);
+          if (tweetText.indexOf('https://t.co/') >= 0 ) {
+            // console.log('this has an image');
+            // console.log(tweetText.indexOf('https://t.co/'));
+            var tweetTextIndexImage = tweetText.indexOf('https://t.co/');
+            var imageHtml = tweetText.slice(tweetTextIndexImage, -1);
+            console.log(imageHtml);
+            var tweetHtml =
+              '  <div class="tweet">' +
+              '  <a href='+ imageHtml + '"/>' + imageHtml + '</a>' +
+              '    <h5>' + tweetText + '</h5>' +
+              '  </div>'
+
+          } else {
+            // console.log('NO image');
+            var tweetHtml =
+              '  <div class="tweet">' +
+              '    <h5>' + tweetText + '</h5>' +
+              '  </div>'
+          };
+
+
+
+
+          // console.log(hitsArray[tweetIndex].text);
+          $('#tweets').append(tweetHtml);
         }
 
-        $('#tweets').css( 'background-color', 'red' );
+        // $('#tweets').css( 'background-color', 'red' );
 
       })
       .catch(function searchFailure(err) {
